170e0e6c03989bc8319694cc1cc58ffc433b89b7
Muhammad Bilal <mbilal45422@gmail.com>
2024-05-24 10:15:38 +0500

    navbar completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl text-center text-orange-500 font-bold'>Hello world</h1>
-      <h1 className='text-6xl text-center text-orange-500 font-bold'>Hello world</h1>
-      <h1 className='text-6xl text-center text-orange-500 font-bold'>Hello world</h1>
-      <h1 className='text-6xl text-center text-orange-500 font-bold'>Hello world</h1>
-      <h1 className='text-6xl text-center text-orange-500 font-bold'>Hello world</h1>
+      <Navbar/>
     </>
   )
 }
